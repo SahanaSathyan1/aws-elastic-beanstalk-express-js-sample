2881357d0165edfbe3fe7d6ee4a047a4ae916fb2
unknown <sahanasathyan1@gmail.com>
2025-02-24 14:50:07 +0530

    Updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is the latest version'));
+app.get('/', (req, res) => res.send('This is updated'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
